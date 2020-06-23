a39043ae36f5b64c404002549e89cf1cf80bbd33
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-23 02:20:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,16 @@ class Dog
  def name=(dogs_name)
   @this_dogs_name = dogs_name
  end
+ @this_dogs_name
 end
+def bark
+  puts "woof!"
+end
+fido = Dog.new
+fido.name ="Fido"
+puts Fido.name
+puts fido.bark
+
 
 
 
